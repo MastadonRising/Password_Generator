db781acdd7870b79529b7270fce1993f4187eb2b
Luis Sandoval <luissandoval1126@gmail.com>
2020-01-18 18:52:05 -0800

    modal add

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,68 @@
 var copybtn = document.getElementById('copybtn')
+var initbtn = document.getElementById('initbtn')
+var finalbtn = document.getElementById('finalbtn')
 
- copybtn.addEventListener("click", function() {
+ 
+
+initbtn.addEventListener("click", function(event){
+ event.preventDefault()
+var  CHAR_SETS = [
+	[ "Numbers", "0123456789"],
+	[ "Lowercase", "abcdefghijklmnopqrstuvwxyz"],
+	[ "Uppercase", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+	[ "ASCII symbols", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"],
+	[ "Space", " "],
+];
+
+
+
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+copybtn.addEventListener("click", function() {
     
 
     var copyText = document.getElementById("password");
